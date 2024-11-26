59f4e7243e9da8097a27148205d5391e46a64ce4
Abcia2 <gianluca.arobaleno@gmail.com>
2024-11-26 13:06:13 +0100

    Aggiungere i file di progetto.

---
+++ Magazzino TechWare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazzino_TechWare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Il programma deve simulare il lavoro di tre operatori che preparano gli ordini contemporaneamente.
+            
+            Ogni operatore gestisce un determinato numero di ordini in parallelo, e il programma deve monitorare e riportare il tempo impiegato
+            
+             - Ogni operatore prepara da 10 a 20 ordini, ciascuno con un tempo di preparazione casuale tra 200 e 1000 ms.
+
+            Il programma deve:
+
+             - Utilizzare i Task per simulare il lavoro degli operatori.
+             -  Monitorare il progresso in tempo reale(stato degli ordini completati).
+             -  Al termine, visualizzare quanti ordini sono stati completati da ciascun operatore e il tempo totale impiegato per completare tutti gli ordini.
+            
+            Luca richiede un report finale che mostri quale operatore è stato più veloce e quale ha completato il maggior numero di ordini.
+            */
+        }
+    }
+}
