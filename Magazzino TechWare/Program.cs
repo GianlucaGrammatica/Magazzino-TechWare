a542a106b70c16480adf590332656d94a1b10a33
Gianluca <10934204@itisrossi.vi.it>
2024-11-29 09:44:44 +0100

    Forse funziona

--- Magazzino TechWare/Program.cs
+++ Magazzino TechWare/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Magazzino_TechWare
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static Random random = new Random();
+        static async Task Main(string[] args)
         {
             /*
             Il programma deve simulare il lavoro di tre operatori che preparano gli ordini contemporaneamente.
@@ -26,19 +30,65 @@ namespace Magazzino_TechWare
             Luca richiede un report finale che mostri quale operatore è stato più veloce e quale ha completato il maggior numero di ordini.
             */
 
+            Stopwatch stopwatchTotal = Stopwatch.StartNew();
 
+            Task<int[]> Operaio1 = Operaio("Operaio 1");
+            Task<int[]> Operaio2 = Operaio("Operaio 2");
+            Task<int[]> Operaio3 = Operaio("Operaio 3");
+
+            int[][] Risultati = await Task.WhenAll(Operaio1, Operaio2, Operaio3);
+            stopwatchTotal.Stop();
+
+            int OrdiniTotali = Risultati[0][0] + Risultati[1][0] + Risultati[2][0];
+            Console.WriteLine($"\n\tOrdini totali: {OrdiniTotali}\n\tTempo totale: {stopwatchTotal.ElapsedMilliseconds}\n");
+
+            int FastestTime = Risultati[0][1];
+            int FastestId = 1;
+            int MaxValue = Risultati[0][0];
+            int MaxId = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                if(Risultati[i][1] < FastestTime)
+                {
+                    FastestTime = Risultati[i][1];
+                    FastestId = i + 1;
+                }
+
+                if(Risultati[i][0] > MaxValue)
+                {
+                    MaxValue = Risultati[i][0];
+                    MaxId = i + 1;
+                }
+            }
+
+            Console.WriteLine($"\n\tPiù veloce: Operaio {FastestId} - {FastestTime} \n\tPiù ordini fatti: Operaio {MaxId} - {MaxValue}");
+            
+            Console.ReadKey();
         }
 
 
-        static async Task<int> Operaio(string nomeOperaio)
-        {
-            Random random = new Random();
+        static async Task<int[]> Operaio(string nomeOperaio)
+        {            
             int NumeroOrdini = random.Next(10, 21);
             int TempoDiPreparazione = random.Next(200, 1001);
-
             string NomeOperaio = nomeOperaio;
 
-            return NumeroOrdini;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < NumeroOrdini; i++)
+            {
+                Console.WriteLine($"{NomeOperaio} inizia la preparazione di un ordine - {i + 1}/{NumeroOrdini}");
+
+                await Task.Delay(TempoDiPreparazione);
+
+                Console.WriteLine($"{NomeOperaio} ha preparato l'ordine - {i + 1}/{NumeroOrdini}");
+            }
+
+            stopwatch.Stop();
+
+            int[] Returns = { NumeroOrdini, (int)stopwatch.ElapsedMilliseconds };            
+
+            return Returns;
         }
     }
 }
